d529a96645b838e2482cc506c9a4575b00e69a77
Ben Congdon <bencongdon96@gmail.com>
2020-09-13 10:22:03 -0700

    Correctly use db_file; Enable incremental updates

--- miniflux-to-sqlite.sh
+++ miniflux-to-sqlite.sh
@@ -30,7 +30,8 @@ while getopts 'uphd:v' flag; do
   esac
 done
 
-if [ -z "$1" ]; then
+db_file="$1"
+if [ -z "$db_file" ]; then
 	echo "No database file provided."
 	print_usage
 fi
@@ -38,13 +39,16 @@ fi
 connection_str="postgresql://$user:$pass@$host/$database"
 
 echo "Exporting database..."
-db-to-sqlite $connection_str miniflux.db --all --redact entries document_vectors --progress
+db-to-sqlite $connection_str $db_file --all --redact entries document_vectors --progress
 
 echo "Cleaning up removed entries..."
-sqlite-utils query miniflux.db "DELETE FROM entries WHERE status == 'removed';"
+sqlite-utils query $db_file "DELETE FROM entries WHERE status == 'removed';"
 
-echo "Enabling fts..."
-sqlite-utils enable-fts miniflux.db entries content
+has_fts=$(sqlite-utils query $db_file "SELECT name FROM sqlite_master WHERE type='table' AND name='entries_fts';")
+if [[ -z $has_fts ]]; then
+	echo "Enabling fts..."
+	sqlite-utils enable-fts $db_file entries content --create-triggers
+fi
 
 echo "Optimizing db..."
-sqlite-utils optimize miniflux.db
+sqlite-utils optimize $db_file
